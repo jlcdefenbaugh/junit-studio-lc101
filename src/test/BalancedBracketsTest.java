58fdebe6c5d8977ddb1796f1b5b693bd64608c5a
Jami <jamidefenbaugh@gmail.com>
2023-01-26 20:31:30 -0600

    began testing, need to clean up naming and eliminate redundant tests

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -11,6 +12,61 @@ public class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void bracketsAroundOtherCharactersReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[String]"));
+    }
+
+    @Test
+    public void bracketsInsideInBetweenCharactersReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void bracketsBeforeStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]String"));
+    }
+
+    @Test
+    public void emptyStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void stringWithOneBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void stringWithBracketsInTheWrongOrderReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    @Test
+    public void oneBracketWithNoStringReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void oddNumberOfBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Launch]Code["));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]Launch[Code]]"));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[][]LaunchCode[]]"));
+    }
+
+    @Test
+    public void nestedBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Launch[Code]]"));
+    }
+
 
 
 }
